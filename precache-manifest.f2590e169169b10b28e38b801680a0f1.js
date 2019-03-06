13b5872bfe10ea84e6d8bf2a21fbc5bb3396a636
Luis <luismachain95@hotmail.com>
2019-03-05 23:19:55 -0300

    Updates

---
+++ precache-manifest.f2590e169169b10b28e38b801680a0f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c622e281eb2816750957",
+    "url": "/Zooga/static/js/runtime~main.c622e281.js"
+  },
+  {
+    "revision": "5b8b12e4c4ca7c80122f",
+    "url": "/Zooga/static/js/main.5b8b12e4.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/Zooga/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "5b8b12e4c4ca7c80122f",
+    "url": "/Zooga/static/css/main.0d05848b.chunk.css"
+  },
+  {
+    "revision": "a59735d6db74f8e169cbc27e74c36cf5",
+    "url": "/Zooga/index.html"
+  }
+];
